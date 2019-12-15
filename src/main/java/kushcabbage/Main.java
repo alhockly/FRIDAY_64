09544ee20ad2eb39701bbd44aa155ca5b280dad6
Alex Hockly <alhockly@gmail.com>
2019-12-15 12:24:17 +0000

    removed javafx imports

--- src/main/java/kushcabbage/Main.java
+++ src/main/java/kushcabbage/Main.java
@@ -1,15 +1,15 @@
 package kushcabbage;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.scene.control.Label;
+//import javafx.application.Application;
+//import javafx.geometry.Insets;
+//import javafx.scene.Scene;
+//import javafx.scene.layout.Background;
+//import javafx.scene.layout.BackgroundFill;
+//import javafx.scene.layout.CornerRadii;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.paint.Color;
+//import javafx.stage.Stage;
+//import javafx.scene.control.Label;
 
 
 public class Main{ //extends Application {
@@ -18,25 +18,28 @@ public class Main{ //extends Application {
 
 
     public static void main(String[] args) {
+        System.getProperties().list(System.out);
+        if (!System.getProperty("os.name").toLowerCase().contains("windows")){
+            iHotwordClass = new HotwordListener();
+        }
 
-        iHotwordClass = new HotwordListener();
         Thread t = new Thread(iHotwordClass);
         t.start();
         //launch();
     }
 
-    //@Override
-    public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-        StackPane root = new StackPane(l);
-        root.setBackground(new Background(new BackgroundFill(Color.BLACK,  CornerRadii.EMPTY,  Insets.EMPTY)));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
-        stage.show();
-    }
+    //    //@Override
+    //    public void start(Stage stage) throws Exception {
+    //        String javaVersion = System.getProperty("java.version");
+    //        String javafxVersion = System.getProperty("javafx.version");
+    //
+    //        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    //
+    //        StackPane root = new StackPane(l);
+    //        root.setBackground(new Background(new BackgroundFill(Color.BLACK,  CornerRadii.EMPTY,  Insets.EMPTY)));
+    //        Scene scene = new Scene(root);
+    //        stage.setScene(scene);
+    //        stage.setFullScreen(true);
+    //        stage.show();
+    //    }
 }
